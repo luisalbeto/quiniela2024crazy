d4d31bb897c05846843259792b4a1d22664879a1
luisalbeto <luis-alberto11@hotmail.com>
2024-07-01 18:06:11 -0400

    Feat:

--- app/cuartos/page.tsx
+++ app/cuartos/page.tsx
@@ -13,12 +13,7 @@ export default async function Page() {
 	}*/
     return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-sky to-blue overflow-hidden">
-        <section className="flex flex-col items-center text-center">
-            {/* Hero Section with Title */}
-        <h1 className="text-8xl font-bold text-gray-900 ">Fase Final</h1>
-        <p className="mt-4 text-2xl text-gray-800">Predice al Ganador de la Copa America</p> 
-      
-        </section>
+     
         <h1 className="text-6xl font-bold text-gray-900 ">Cuartos de Final</h1>
         <Matches matchDay={4}/>
         <h1 className="text-6xl font-bold text-gray-900 ">Semifinales</h1>
